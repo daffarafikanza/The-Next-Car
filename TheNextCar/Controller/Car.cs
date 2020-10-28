d52d42b6cc02aa7af5de22434afa54e2796a1f59
daffarafikanza <61929389+daffarafikanza@users.noreply.github.com>
2020-10-28 19:51:40 +0700

    Add files via upload

---
+++ TheNextCar/Controller/Car.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNextCar.Controller
+{
+    class Car
+    {
+        private DoorController doorController;
+        private AccubatteryController accubatteryController;
+        private OnCarEngineStateChanged callback;
+
+        public Car(DoorController doorController, AccubatteryController accubatteryController, OnCarEngineStateChanged callback)
+        {
+            this.doorController = doorController;
+            this.accubatteryController = accubatteryController;
+            this.callback = callback;
+        }
+
+        private bool doorIsClosed()
+        {
+            return this.doorController.isClosed();
+        }
+
+        private bool doorIsLocked()
+        {
+            return this.doorController.isLocked();
+        }
+
+        private bool powerIsReady()
+        {
+            return this.accubatteryController.accubatteryIsOn();
+        }
+
+        public void startEngine()
+        {
+            if (!doorIsClosed())
+            {
+                this.callback.onCarEngineStateChanged("STOPED", "Close the door");
+                return;
+            }
+
+            if (!doorIsLocked())
+            {
+                this.callback.onCarEngineStateChanged("STOPED", "Lock the door");
+                return;
+            }
+
+            if (!powerIsReady())
+            {
+                this.callback.onCarEngineStateChanged("STOPED", "no power available");
+                return;
+            }
+
+            this.callback.onCarEngineStateChanged("STOPED", "Engine Started");
+        }
+
+        public void toogleTheLockDoorButton()
+        {
+            if (!doorIsLocked())
+            {
+                this.doorController.activateLock();
+            }
+            else
+            {
+                this.doorController.unlock();
+            }
+        }
+
+        public void toogleTheOpenDoorButton()
+        {
+            if (!doorIsClosed())
+            {
+                this.doorController.close();
+            }
+            else
+            {
+                this.doorController.open();
+            }
+        }
+
+        public void tooglePowerButton()
+        {
+            if (!powerIsReady())
+            {
+                this.accubatteryController.turnOn();
+            }
+            else
+            {
+                this.accubatteryController.turnOff();
+            }
+        }
+    }
+
+    interface OnCarEngineStateChanged
+    {
+        void onCarEngineStateChanged(string value, string message);
+    }
+}
---
+++ TheNextCar/Controller/DoorController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheNextCar.Model;
+
+namespace TheNextCar.Controller
+{
+    class DoorController
+    {
+        private Door door;
+        private OnDoorChanged callbackOnDoorChanged;
+
+        public DoorController(OnDoorChanged callbackOnDoorChanged)
+        {
+            this.callbackOnDoorChanged = callbackOnDoorChanged;
+            this.door = new Door();
+        }
+
+        public void close()
+        {
+            this.door.close();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("CLOSED", "door closed");
+        }
+
+        public void open()
+        {
+            this.door.open();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("OPENED", "door opened");
+        }
+
+        public void activateLock()
+        {
+            this.door.activateLock();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("LOCKED", "door locked");
+        }
+
+        public void unlock()
+        {
+            this.door.unlock();
+            this.callbackOnDoorChanged.onDoorOpenStateChanged("UNLOCKED", "door unlocked");
+        }
+
+        public bool isClosed()
+        {
+            return this.door.isClosed();
+        }
+
+        public bool isLocked()
+        {
+            return this.door.isLocked();
+        }
+    }
+
+    interface OnDoorChanged
+    {
+        void onlockDoorStateChanged(string value, string message);
+        void onDoorOpenStateChanged(string value, string message);
+    }
+}
